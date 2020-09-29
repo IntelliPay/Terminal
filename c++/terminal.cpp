d31ad47248a7a0ca6716a0633e6bbe3d411b7c1b
levi stanley <levi@diracian.com>
2020-09-29 16:24:20 -0400

    changing from only support POSIX, to supporting windows, linux and macOS

--- c++/terminal.cpp
+++ c++/terminal.cpp
@@ -1,45 +1,22 @@
 #include "terminal.h"
 
 
-extern bool init(string terminal_type = "v400c-plus", int baud_rate = 9600, int byte_size = 8, bool stop_bit = true, bool xonxoff = false, bool rtscts = false, string port = "", int timeout = 0) {
+extern bool init(string terminal_type = "v400c-plus", int baud_rate = 9600, serial::bytesize_t byte_size = serial::eightbits, serial::stopbits_t stop_bit = serial::stopbits_one, serial::flowcontrol_t xonxoff = serial::flowcontrol_none, bool rtscts = false, string port = "", int timeout = 0) {
     try {
         intellipay_terminal_type = terminal_type;
         intellipay_serial_port = port;
 
         // let's set up the connection to the terminal device.
-        intellipay_port = open(intellipay_serial_port.c_str(), O_RDWR);
-        if (tcgetattr(intellipay_port, &stty) != 0) {
-            cout << "Error " << errno << " from tcgetattr: " << strerror(errno) << endl;
+        stty.setPort(intellipay_serial_port);
+        stty.setBaudrate(baud_rate);
+        stty.setStopbits(stop_bit);
+        stty.setBytesize(byte_size);
+        stty.setFlowcontrol(xonxoff);
+        stty.setRTS(rtscts);
+        stty.open();
+        if (!stty.isOpen()) {
+            return false;
         }
-
-        // lets set up the stty
-
-        stty.c_cflag &= ~PARENB; // Clear parity bit
-        stty.c_cflag &= (stop_bit) ? CSTOPB : ~CSTOPB; // Clear stop field
-        stty.c_cflag |= CS8; // 8 bits per byte  -- TODO: need to allow users to change this parameter.
-        stty.c_cflag &= (rtscts) ? CRTSCTS : ~CRTSCTS; // Disable RTS/CTS hardware flow control
-        stty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
-
-        stty.c_lflag &= ~ICANON;
-        stty.c_lflag &= ~ECHO; // Disable echo
-        stty.c_lflag &= ~ECHOE; // Disable erasure
-        stty.c_lflag &= ~ECHONL; // Disable new-line echo
-        stty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-        stty.c_iflag &= (xonxoff) ? (IXON | IXOFF | IXANY) : ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-        stty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR |
-                          ICRNL); // Disable any special handling of received bytes
-
-        stty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
-        stty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-        // stty.c_oflag &= ~OXTABS; // Prevent conversion of tabs to spaces (NOT PRESENT ON LINUX)
-        // stty.c_oflag &= ~ONOEOT; // Prevent removal of C-d chars (0x004) in output (NOT PRESENT ON LINUX)
-
-        stty.c_cc[VTIME] = timeout;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
-        stty.c_cc[VMIN] = 0;
-
-        // Set in/out baud rate to be 9600
-        cfsetispeed(&stty, (baud_rate == 9600) ? B9600 : (baud_rate == 115200) ? B115200 : B19200);
-        cfsetospeed(&stty, (baud_rate == 9600) ? B9600 : (baud_rate == 115200) ? B115200 : B19200);
     } catch (const exception e) {
         return false;
     }
@@ -66,23 +43,15 @@ extern json process(string action, float amount, json options = json({})) {
     json response;
     response["status"] = "failed";
 
-    // Let's save and validate
-    if (tcsetattr(intellipay_port, TCSANOW, &stty) != 0) {
-        cout << "Error " << errno << " from tcsetattr: " << strerror(errno) << endl;
-    }
+    stty.write(obj.dump());
 
-    const char* str = obj.dump().c_str();
-    write(intellipay_port, str, strlen(str));
-
-    char read_buffer[256];
-    memset(&read_buffer, '\0', sizeof(read_buffer));
-    int number_of_bytes = read(intellipay_port, &read_buffer, sizeof(read_buffer));
-    if (number_of_bytes < 0) {
+    string read_buffer;
+    stty.readline(read_buffer, 256, "\n");
+    if (!read_buffer.length()) {
         response["message"] = "Error reading from terminal device.";
     }
 
-    string terminal_raw_response = string(read_buffer);
-    json terminal_response = json::parse(terminal_raw_response);
+    json terminal_response = json::parse(read_buffer);
 
     if (terminal_response.contains("Response")) {
         if (terminal_response["Response"].contains("PaymentID")) {
--- c++/terminal.h
+++ c++/terminal.h
@@ -5,8 +5,14 @@
 #include "nlohmann/json.hpp"
 #include <fcntl.h>
 #include <termios.h>
-#include <unistd.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include "serial/serial.h"
 #include <iostream>
 #include <boost/algorithm/string.hpp>
 #include <boost/format.hpp>
@@ -17,7 +23,8 @@ using namespace nlohmann;
 extern string intellipay_terminal_type;
 extern int intellipay_port;
 extern string intellipay_serial_port;
-extern struct termios stty;
+
+serial::Serial stty;
 
 extern bool init(string, int, int, int, bool, bool, string, int);
 extern json process(string, json);
