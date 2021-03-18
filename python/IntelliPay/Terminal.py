56d17f4c7ea5f6ecb98f9cae57ffa9deb2388b0b
levi stanley <levi@eneservices.com>
2021-03-18 10:59:01 -0400

    changed default baud rate.

--- python/IntelliPay/Terminal.py
+++ python/IntelliPay/Terminal.py
@@ -1,3 +1,6 @@
+#! /usr/bin/env python3
+
+import time
 import serial
 import json
 
@@ -23,15 +26,6 @@ class Terminal:
         self.terminal.port = port
         self.terminal.parity = serial.PARITY_NONE
 
-    def prep_message(self, message):
-        message = message + str(chr(3))
-        lrc = ord(message[0])
-        for i in range(1, len(message)):
-            lrc ^= ord(message[i])
-        message =  str(chr(2)) + message
-        prep = message + str(chr(lrc))
-        return prep
-
     def process(self, action, amount, options=None):
         action = action.upper()
         acceptable_action_types = ["SALE", "REFUND", "VOID"]
@@ -42,8 +36,9 @@ class Terminal:
         if action == "VOID" and "PaymentID" not in options:
             raise Exception(action + " requires a PaymentID")
 
+        request_json = {}
         request_json["Operation"] = action
-        request_json = {"Amount": amount}
+        request_json["Amount"] = amount
 
         if options is not None:
             for field in ["Tip", "Invoice", "Clerk", "PaymentItem", "PaymentID"]:
@@ -59,22 +54,30 @@ class Terminal:
 
         result = {"status": "failed"}
         response = {}
-        if self.terminal.is_open:
-            self.terminal.flushInput()
-            self.terminal.flushOutput()
-            self.terminal.write(bytes(self.prep_message(json.dumps(request_json)), 'ascii'))
-            while True:
-                data = self.terminal.readline()
-                if len(data) > 0:
-                    response = json.loads(self.terminal.readline().decode('ascii'))
-                    break
+        if self.terminal.isOpen:
+            try:
+                self.terminal.flushInput()
+                self.terminal.flushOutput()
+                message = self.prep_message(json.dumps(request_json))
+                self.terminal.write(bytes(message, 'ascii'))
+                while True:
+                    data = self.terminal.readline()
+                    if len(data) > 0:
+                        print("Data: {}".format(data))
+                        response = json.loads(data.decode('ascii')[1:-2])
+                        break
+                    time.sleep(2)
+            except Exception as ex:
+                print("*E* {}".format(e.message))
             self.terminal.close()
         if "Response" in response:
             response = response["Response"]
             if "PaymentID" in response:
                 result["payment-id"] = response["PaymentID"]
             if "ResponseCode" in response and response["ResponseCode"] == "00":
-                result["status"] = "processed"  # TODO: talk to matt probably should used "approved" instead.
+                result["status"] = "approved" 
+            elif "ResponseCode" in response and response["ResponseCode"] == "99":
+                result["message"] = response["ResponseMsg"]
             if "Operation" in response:
                 if response["Operation"] == "SALE" or response["Operation"] == "REFUND":
                     result["card-information"] = dict()
@@ -82,6 +85,14 @@ class Terminal:
                     result["card-information"]["last4"] = response["Card"]["Last4"]
                     result["card-information"]["expiration"] = response["Card"]["ExpDate"]
         return result
+
+    def prep_message(self, message):
+        message =  message + str(chr(3)) 
+        lrc = ord(message[0])
+        for i in range(1, len(message)):
+            lrc ^= ord(message[i])
+        prep = str(chr(2)) + message + str(chr(lrc))
+        return prep
 
     def sale(self, amount, options=None):
         return self.process("SALE", amount, options)
@@ -91,3 +102,4 @@ class Terminal:
 
     def void(self, amount, options=None):
         return self.process("VOID", amount, options)
+
